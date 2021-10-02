e0f3164450c8d3920804e3b0be56a4f7e453ef8d
deeschaedler <deeschaedler@gmail.com>
2021-10-02 14:27:47 -0400

    Removed copyright notice work is not registered with the copyright office

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,5 @@
 # Teeny Tiny Trains
-# Copyright 2021 Dee Schaedler
+# By Dee Schaedler
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
